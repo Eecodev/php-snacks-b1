9248029e493ef310d4ff72008fd642aa46880fd8
Eecodev <mnasser.bbk@gmail.com>
2023-11-28 15:31:04 +0100

    snack 1 completed

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+
+    $games = [
+        [
+            'in_house_team' => 'Chicago Bulls',
+            'foreign_team' => 'Boston Celtics',
+            'in_house_score' => '65',
+            'foreign_score'=> '40',
+        ],
+        [
+            'in_house_team' => 'Miami Heat',
+            'foreign_team' => 'Orlando Magic',
+            'in_house_score' => '35',
+            'foreign_score'=> '70',
+        ],
+        [
+            'in_house_team' => 'New York Knicks',
+            'foreign_team' => 'Toronto Raptors',
+            'in_house_score' => '46',
+            'foreign_score'=> '55',
+        ],
+        [
+            'in_house_team' => 'Detroit Pistons',
+            'foreign_team' => 'Indiana Pacers',
+            'in_house_score' => '83',
+            'foreign_score'=> '76',
+        ],
+    ]
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +40,25 @@
     <title>Snacks B1</title>
 </head>
 <body>
+    <div class="container">
+        <header>
+            <h1>NBA SCORES</h1>
+        </header>
+        <main>
+        <?php
+        foreach($games as $game){
+            echo 
+            "<div> 
+                {$game['in_house_team']} 
+                - {$game['foreign_team']} 
+                | {$game['in_house_score']} 
+                - {$game['foreign_score']}
+            </div>";
+            }
+        ?>
+        </main>  
+    </div>
     
+
 </body>
 </html>
