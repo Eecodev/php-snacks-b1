2c88260f5dfc4bc174b2858002d68fb5987537f0
Eecodev <mnasser.bbk@gmail.com>
2023-11-28 16:42:14 +0100

    file created and if loop applied to form

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+// snack 2
+    $contact_details = [
+        [
+            'name' => 'Mahdi',
+            'email' => 'mahdijk7@hotmail.it',
+            'age' => '29',
+        ]
+    ];
+
+        if(isset($_GET['name']) && strlen($_GET['name']) > 3 && filter_var($_GET['email'], FILTER_VALIDATE_EMAIL) && is_numeric($_GET['age'])){
+            echo 'Access enabled';   
+        } else {
+            echo 'Access denied';
+        }
+?>
